7026fcc9d402c5182d7bda1fb745ee63b4a51009
Alexey Frunza <tvoih8r@mail.ru>
2021-08-27 15:17:20 +0300

    add iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_6ec56c0cf331473ea205a67e0cbbe5ce',
-                                  secret_token: 'Tsk_f98275855a9a4552bd1a1750a892a689',
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  secret_token: Rails.application.credentials.iex_client[:secret_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     client.price(ticker_symbol)
   end
